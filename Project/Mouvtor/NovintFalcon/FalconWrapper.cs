b7e5c5ae6c70538fc267257023be2d79c5ee7418
Dimitri Lizzi <araxor@gmail.com>
2015-12-16 12:48:37 +0100

    Added first draft of the novint falcon input device.

---
+++ Project/Mouvtor/NovintFalcon/FalconWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovintFalcon
+{
+    public static class FalconWrapper
+    {
+        const string falcon = "Falcon Wrapper";
+
+        [DllImport(falcon)]
+        public static extern void StartHaptics();
+        [DllImport(falcon)]
+        public static extern void StopHaptics();
+        [DllImport(falcon)]
+        public static extern bool IsDeviceCalibrated();
+        [DllImport(falcon)]
+        public static extern bool IsDeviceReady();
+        [DllImport(falcon)]
+        public static extern double GetXPos();
+        [DllImport(falcon)]
+        public static extern double GetYPos();
+        [DllImport(falcon)]
+        public static extern double GetZPos();
+        [DllImport(falcon)]
+        public static extern void SetServo(double[] speed);
+        [DllImport(falcon)]
+        public static extern void SetServoPos(double[] pos, double strength);
+    }
+}
