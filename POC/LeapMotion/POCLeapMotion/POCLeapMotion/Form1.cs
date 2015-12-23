0c7c3338dc21e24daa0150a5cf147615cdc12644
LucienCamuglia <lucien.camuglia@gmail.com>
2015-12-23 09:28:44 +0100

    finalization of leap as IInputDevice

--- POC/LeapMotion/POCLeapMotion/POCLeapMotion/Form1.cs
+++ POC/LeapMotion/POCLeapMotion/POCLeapMotion/Form1.cs
@@ -14,13 +14,15 @@ namespace POCLeapMotion
 {
     public partial class Form1 : Form
     {
-        const int FINGER_ID = 1; //0=thumb / 1=index / 2=major / 3=annular / 4=auricular
-        //leap motion controller
-        Controller controller;
+        /* const int FINGER_ID = 1; //0=thumb / 1=index / 2=major / 3=annular / 4=auricular
+         //leap motion controller
+         Controller controller;*/
+        LeapMotion monleap;
         bool first = true;
         bool Start = false;
+        bool canDraw = false;
         Graphics graphics;
-        Point OldPos;
+        PointF OldPos;
 
         public Form1()
         {
@@ -38,15 +40,16 @@ namespace POCLeapMotion
                 tmr.Enabled = false;
                 Start = false;
                 BtnStartStop.Text = "Start";
-        
-
             }
             else
             {
                 try
                 {
                     first = true;
-                    controller = new Controller();
+                    /*controller = new Controller();*/
+                    monleap = new LeapMotion();
+                    monleap.StartDrawing += monleap_StartDrawing;
+                    monleap.StopDrawing += monleap_StopDrawing;
                     Start = true;
                     BtnStartStop.Text = "Stop";
                     tmr.Enabled = true;
@@ -59,22 +62,58 @@ namespace POCLeapMotion
             }
         }
 
+        void monleap_StopDrawing(object sender, EventArgs e)
+        {
+            canDraw = false;
+        }
+
+        void monleap_StartDrawing(object sender, EventArgs e)
+        {
+            canDraw = true;
+        }
+
         private void tmr_Tick(object sender, EventArgs e)
         {
-            if (controller == null || !controller.IsConnected) return;
-            Draw(BtnStartStop, controller.Frame());
+            /*if (controller == null || !controller.IsConnected) return;
+            Draw(BtnStartStop, controller.Frame());*/
+            Draw();
 
         }
 
-        private void Draw(Button btn, Frame frame)
+        private void Draw(/*Button btn, Frame frame*/)
         {
-
-            var hands = frame.Hands;
-            var hand = hands[0];
-            var fingers = hand.Fingers;
             var pen = new Pen(Color.Red);
 
-            if ((hands[0].IsRight) && (fingers[FINGER_ID] != Finger.Invalid) && (fingers[FINGER_ID].IsExtended))
+            if (canDraw)
+            {
+                label1.Text = "X : " + monleap.CurrentNormalizedPosition.X.ToString();
+                label2.Text = "Y : " + monleap.CurrentNormalizedPosition.Y.ToString();
+                label3.Text = "Z : " + monleap.CurrentNormalizedPosition.Z.ToString();
+
+                if (first)
+                {
+                    OldPos = monleap.CurrentNormalizedPosition.ToPointF();
+                    OldPos.X = OldPos.X * this.Height;
+                    OldPos.Y = OldPos.Y * this.Width;
+                    first = false;
+                }
+                else
+                {
+                    PointF newPos = monleap.CurrentNormalizedPosition.ToPointF();
+                    newPos.X = newPos.X * this.Height;
+                    newPos.Y = newPos.Y * this.Width;
+                    pen.Width = (float)monleap.CurrentNormalizedPosition.Z * 10;
+                    graphics.DrawLine(pen, OldPos, newPos);
+                    OldPos = newPos;
+                }
+
+            }
+
+            /*var hands = frame.Hands;
+            var hand = hands[0];
+            var fingers = hand.Fingers;
+             
+             if ((hands[0].IsRight) && (fingers[FINGER_ID] != Finger.Invalid) && (fingers[FINGER_ID].IsExtended))
             {
                 var normailizedPos = frame.InteractionBox.NormalizePoint(fingers[FINGER_ID].StabilizedTipPosition, false);
 
@@ -101,7 +140,10 @@ namespace POCLeapMotion
                     graphics.DrawLine(pen, OldPos, newPos);
                     OldPos = newPos;
                 }
-            }
+            }*/
+
+
+
         }
     }
 }
---
+++ POC/LeapMotion/POCLeapMotion/POCLeapMotion/IInputDevice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouvtorCommon
+{
+    public interface IInputDevice
+    {
+        #region Properties
+        Point3DNormalized CurrentNormalizedPosition { get; }
+        #endregion
+
+        #region Events
+        event EventHandler StartDrawing;
+        event EventHandler StopDrawing;
+        #endregion
+    }
+}
