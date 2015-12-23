b768d6ab054f848dd98ae75cb47c030ec3dc5440
Dimitri Lizzi <araxor@gmail.com>
2015-12-23 08:25:03 +0100

    Added Point3DNormalized class that was forgotten in last commit

--- Project/Mouvtor/MouvtorCommon/Point3DNormalized.cs
+++ Project/Mouvtor/MouvtorCommon/Point3DNormalized.cs
@@ -1,23 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MouvtorCommon
 {
-    //public struct Point3DNormalized
-    //{
-    //    public readonly float X { get; internal set;}
-    //    public readonly float Y { get; internal set; }
-    //    public readonly float Z { get; internal set; }
+    public struct Point3DNormalized
+    {
+        private double _x;
+        private double _y;
+        private double _z;
 
-    //    public Point3DNormalized(float x, float y, float z)
-    //    {
-    //        X = x;
-    //        Y = y;
-    //        Z = z;
-    //    }
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
 
-    //}
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public double Z
+        {
+            get { return _z; }
+            set { _z = value; }
+        }
+
+        public Point3DNormalized(double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public PointF ToPointF()
+        {
+            return new PointF((float)this.X, (float)this.Y);
+        }
+
+    }
 }
