57114ad9eaf6a563e6adc6d9d10c891fff562ee6
Dimitri Lizzi <araxor@gmail.com>
2015-12-23 08:40:15 +0100

    Added a class to represent a step in the path (3d point + timestamp)

---
+++ Project/Mouvtor/MouvtorCommon/PathStep.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouvtorCommon
+{
+    public class PathStep
+    {
+        public Point3DNormalized NormalizedPosition { get; protected set; }
+        public long Timestamp { get; internal set; }
+
+        public PathStep(Point3DNormalized fromPoint, long timestamp) {
+            NormalizedPosition = fromPoint;
+            Timestamp = timestamp;
+        }
+
+        public PathStep(double x, double y, double z, long timestamp)
+            : this(new Point3DNormalized(), timestamp)
+        { }
+    }
+}
